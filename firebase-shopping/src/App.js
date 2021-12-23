0f0f96b53d9e54f94dcad44fa52f1b50c6847f0d
Gorka <gorkavillara@gmail.com>
2021-12-23 12:10:58 +0100

    Sesión 20 - Autenticación con Firebase

--- firebase-shopping/src/App.js
+++ firebase-shopping/src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { app } from './firebase'
 import Header from './components/Header';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Register from './routes/Register';
+import { Toaster } from 'react-hot-toast';
+
+export const AppContext = createContext(null);
 
 function App() {
+  const [route, setRoute] = useState("home")
+  const [user, setUser] = useState(null);
+  console.log(user);
   return (
-    <>
+    <AppContext.Provider value={{ route, setRoute, user, setUser }}>
+      <Toaster />
       <Header />
       <main className="p-6">
-        Bienvenid@ a FireShopping
+        {route === "home" && <Home />}
+        {route === "login" && <Login />}
+        {route === "register" && <Register />}
+        {user && <p>Usuario logueado: {user.email}</p>}
       </main>
-    </>
+    </AppContext.Provider>
   );
 }
 
---
+++ firebase-shopping/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Esta es la home
+        </div>
+    )
+}
+
+export default Home
